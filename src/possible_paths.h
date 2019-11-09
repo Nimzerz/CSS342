0d8293b9c83c5837f371cc878f4a3fae8f9cfbab
Nimzerz <aminaimi14@gmail.com>
2019-11-08 22:17:46 -0800

    final homework commit2

--- src/possible_paths.h
+++ src/possible_paths.h
@@ -11,22 +11,28 @@ long possiblePathsRecursive(int M, int N) {
 }
 
 long possiblePathsIterative(int M, int N) {
-    // homework
+    // if there is no grid, there are no paths
     if (M == 0 || N == 0) {
         return 0;
     }
-    long paths [M * N];
+    long paths [M * N]; // stores the number of paths to every location on the grid
+
+    // first row of the grid only has 1 path
     for (int column = 0; column < M; column++) {
         paths[column] = 1;
     }
+
+    // computes the number of paths to every square on the grid
     for (int row = 1; row < N; row++) {
         for (int column = 0; column < M; column++) {
             if (column == 0) {
-                paths[M * row] = 1;
+                paths[M * row] = 1; // first column of the grid only has 1 path
             } else {
-                paths[M * row + column] = paths[M * (row - 1) + column] + paths[M * row + (column - 1)];
+                // num of paths found by adding num of paths to top and left adjacent squares
+                paths[M * row + column] = paths[M * (row - 1) + column] +
+                                          paths[M * row + (column - 1)];
             }
         }
     }
-    return paths[M * N - 1];
+    return paths[M * N - 1]; // returns the total number of paths to the bottom-right corner
 }
